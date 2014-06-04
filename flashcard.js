3ffbb851b8b54d0c330f9a9c0f62292b9fbb3d56
fredrick-dominy <fredrickdominy@gmail.com>
2014-06-04 16:06:22 -0700

    further refinement of flashcard html, css, and js files -FMD

--- flashcard.js
+++ flashcard.js
@@ -1,17 +1,17 @@
 // hide all flashcards except the first one
 $(function() {
 
-
-    $('div').on('click', '.answer2', function(){
-      $('.answer2').effect('shake', 'slow');
-    });
-
   $('.side2').hide();
 
+    $('div').on('click', '.answer2', function(){
+      $('.answer2').effect('shake', 'slow').css('background-color','red');
+    });
+
+
   $('body').on('click', '.answer1', function() {
+      $('.answer1').css('background-color','green');
       $('.side1').hide(200);
       $('.side2').show(200);
-      $('.motivation').show(1000);
   })
 
   for (var i = 1; i < 4; i++) {

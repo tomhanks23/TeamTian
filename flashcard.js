1c44fab2d3bba3d2172060c2b621127d53fea38c
fredrick-dominy <fredrickdominy@gmail.com>
2014-06-04 15:33:48 -0700

    updated flashcard files

--- flashcard.js
+++ flashcard.js
@@ -1,21 +1,24 @@
 // hide all flashcards except the first one
 $(function() {
 
+
+    $('div').on('click', '.answer2', function(){
+      $('.answer2').effect('shake', 'slow');
+    });
+
   $('.side2').hide();
 
-  $('body').on('click', '.side1', function() {
-      $(this).hide(200);
+  $('body').on('click', '.answer1', function() {
+      $('.side1').hide(200);
       $('.side2').show(200);
+      $('.motivation').show(1000);
   })
-  
-  $('body').on('click', '.side2', function() {
-    $(this).hide(200);
-    $('.side1').show(200);
-  }); 
 
   for (var i = 1; i < 4; i++) {
     console.log(i);
-    
   };
+
+
+    
 
 });

44dd7484331e96fc0300f3fc4d42ea0d1ec07269
fredrick-dominy <fredrickdominy@gmail.com>
2014-06-05 16:05:17 -0700

    Functionality of flashcard page success! -FMD

--- js/database.js
+++ js/database.js
@@ -1,28 +1,33 @@
 var deck = {
   card1: {
     deckName: 'animals',
-    side1_text: '',
-    side2_text: 'Lynx',
+    side1_text: 'What is this animal?',
+    side2_text: 'Lynx use their vision to hunt, but are helped in this activity by excellent hearing. Lynx usually sneak up on prey to within a few short bounds, and then pounce.',
     image_link1: 'http://upload.wikimedia.org/wikipedia/commons/6/68/Lynx_lynx_poing.jpg',
-    image_link2: 'http://upload.wikimedia.org/wikipedia/commons/6/68/Lynx_lynx_poing.jpg'
+    image_link2: 'http://upload.wikimedia.org/wikipedia/commons/6/68/Lynx_lynx_poing.jpg',
+    right_answer: 'Lynx',
+    wrong_answer: 'Ocelot'
   },
 
   card2: {
     deckName: 'animals',
-    side1_text: '',
+    side1_text: 'What is this animal?',
     side2_text: 'Hyena',
     image_link1: 'http://upload.wikimedia.org/wikipedia/commons/c/c3/Spotted_Hyena_at_White_River.jpg',
-    image_link2: 'http://upload.wikimedia.org/wikipedia/commons/c/c3/Spotted_Hyena_at_White_River.jpg'
+    image_link2: 'http://upload.wikimedia.org/wikipedia/commons/c/c3/Spotted_Hyena_at_White_River.jpg',
+    right_answer: 'Hyena',
+    wrong_answer: 'Water Buffalo'
   },
 
   card3: {
     deckName: 'animals',
-    side1_text: '',
+    side1_text: 'What is this animal?',
     side2_text: 'Platypus',
-    image_link1: '',
-    image_link2: ''
+    image_link1: 'http://images.nationalgeographic.com/wpf/media-live/photos/000/006/cache/platypus_662_600x450.jpg',
+    image_link2: 'http://images.nationalgeographic.com/wpf/media-live/photos/000/006/cache/platypus_662_600x450.jpg',
+    right_answer: 'Platypus',
+    wrong_answer: 'Camel'
   }
 
 };
 
-console.log(deck.card3.side2_text);
--- js/flashcard.js
+++ js/flashcard.js
@@ -1,24 +1,57 @@
 // hide all flashcards except the first one
 $(function() {
+//  display each card side 1 first, one after the next. 
+// console.log(deck.card1.side2_text);
 
-  $('.side2').hide();
+// for ( var key in deck) {
+//     var card = deck[key];
+//    for ( var property in card) {
+//     console.log(card[property]);
+//    };
+// };
+var count = 0;
+  var nextCard = function (count) {
+        var newCount = count + 1;
+        var card = deck["card"+newCount];
+        console.log(newCount);
 
-    $('div').on('click', '.answer2', function(){
-      $('.answer2').effect('shake', 'slow').css('background-color','red');
-    });
+        $('.side2').hide();
+        $('.side1').show();
 
+        $('div').on('click', '.answer2', function(){
+          $('.answer2').effect('shake', 'slow');
+        });
 
-  $('body').on('click', '.answer1', function() {
-      $('.answer1').css('background-color','green');
-      $('.side1').hide(200);
-      $('.side2').show(200);
-  })
+        $('body').on('click', '.answer1', function() {
+            $('.side1').hide(200);
+            $('.side2').show(200);
+        })
 
-  for (var i = 1; i < 4; i++) {
-    console.log(i);
+        $('  .side1   ').html(
+           '   <p class="side1-text">   '
+          + card.side1_text
+          + '   </p><img class="big" src="   '
+          + card.image_link1
+          + '  " alt=""></div>    '
+          );
+
+        $('.side2').html('<img class="small" src="'
+          + card.image_link2
+          + '  " alt="">'
+          + ' <div><p class="side2-text">'
+          + card.side2_text
+          + '</p></div> ' 
+          );
+
+        $('.answer1').html(card.right_answer);
+        $('.answer2').html(card.wrong_answer);
   };
 
+  $('body').on('click', '.go', function() {
+    count++;
+    nextCard(count);
+  });
 
-    
+  nextCard(0);
 
 });

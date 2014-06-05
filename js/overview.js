e5328111166e8983730442764fa0c424a8c3ad7b
Fredrick Dominy <fredrickdominy@gmail.com>
2014-06-04 23:20:58 -0700

    Summarized overview.js for further review - FMD

--- js/overview.js
+++ js/overview.js
@@ -1,37 +1,47 @@
 $(function() {
 
-    var card = {};
+
+  // create an empty card object
+  var card = {};
 
 
-    $(".add").on("click", function() {
-        $(".fc_front").val("");
-        $(".fc_back").val("");
+  // select class-'add' and run this function
+    // GET the class='fc_front' value
+    // GET the class='.fc_back Value'
+  $(".add").on("click", function() {
+    $(".fc_front").val();
+    $(".fc_back").val();
+  });
+
+
+  // select class='submit' and run this function
+    // set variables and collect their values
+  $(".submit").on("click", function() {
+    var fc_front_text = $(".fc_front").val();
+    var fc_back_text = $(".fc_back").val();
+
+
+    // Print a list item with the following text and variables
+      // make a PLAY button and stick it on the ass-end of the class='container'
+    $("<li>").text("Front: [" + fc_front_text + "] " + "Back: [" + fc_back_text + "] ")
+      .append("<button>Play</button")
+      .appendTo(".container");
     });
 
 
-    $(".submit").on("click", function() {
-        
 
-        var fc_front_text = $(".fc_front").val();
-        var fc_back_text = $(".fc_back").val();
-        
-        $("<li>").text("Front: [" + fc_front_text + "] " + "Back: [" + fc_back_text + "] ")
-                   .append("<button>Play</button")
-                   // .append("<button>Del</button")
-                   // .addClass(item)
-                   .appendTo(".container");
-
-    });
-
+    //When you click on the list item button run this function
+      //open the linked page (flashcard.html)
     $(".container").on("click", "li button", function() {
         window.location.href = "flashcard.html";
     });
 
 
+    //Run this function when you click on the class='test'
+      // SUPPOSEDLY you can load an image file and place it in the 
+      // specified area...
     $(".test").on("click", function() {
-
       var reader = new FileReader();
-
       reader.onload = (function(e){
         var img = new Image();
         img.src = e.target.result;
@@ -45,5 +55,6 @@ $(function() {
       // $("#addImg_front").files[0].appendTo($(".fc_front"));
       $(".fc_front").css("background-image", document.getElementById('addImg_front').files[0]);
       
-    })
-})
+    });
+
+});

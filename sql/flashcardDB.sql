92f8f48acf2592d91c13e14bc7690ec2fd1bce1a
Tom <tom@mademacbook-air.apollosrc.root>
2014-06-06 15:50:20 -0700

    creat tables

--- sql/flashcardDB.sql
+++ sql/flashcardDB.sql
@@ -1,13 +1,32 @@
-CREATE TABLE `user` (
- `user_id` int(11) NOT NULL AUTO_INCREMENT,
- `first_name` varchar(50) DEFAULT NULL,
- `email` varchar(60) DEFAULT NULL,
- `password` char(20) DEFAULT NULL,
- PRIMARY KEY (`user_id`)
+CREATE TABLE user (
+ user_id int(11) NOT NULL AUTO_INCREMENT,
+ first_name varchar(50) DEFAULT NULL,
+ email varchar(60) DEFAULT NULL,
+ password char(20) DEFAULT NULL,
+ PRIMARY KEY (user_id)
+);
+
+
+CREATE TABLE flashcard_deck (
+ flashcard_deck_id int(11) NOT NULL AUTO_INCREMENT,
+ flashcard_deck_name varchar(50) DEFAULT NULL,
+ user_id int(11) DEFAULT NULL,
+ PRIMARY KEY (flashcard_deck_id)
+);
+
+
+CREATE TABLE flashcard (
+ flashcard_id int(11) NOT NULL AUTO_INCREMENT,
+ flashcard_deck_id int(11) DEFAULT NULL,
+ front_text varchar(100) DEFAULT NULL,
+ back_text varchar(100) DEFAULT NULL,
+ front_image LONGBLOB NOT NULL, 
+ back_image LONGBLOB NOT NULL, 
+ PRIMARY KEY (flashcard_id)
 );
 
 
 -- cat dumps a file
-cat /Users/FMD/Sites/TeamTian/sql/flashcardDB.sql | mysql -h rockit.ccyd2ljjwzet.us-east-1.rds.amazonaws.com -u teamtian -p teamtian
+cat /Library/WebServer/Documents/TeamTian/sql/flashcardDB.sql | mysql -h rockit.ccyd2ljjwzet.us-east-1.rds.amazonaws.com -u teamtian -p teamtian
 
 -h rockit.ccyd2ljjwzet.us-east-1.rds.amazonaws.com -u teamtian -p

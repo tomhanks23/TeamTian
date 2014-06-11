06fe98969ba7a990295351b5faaf299c6bc26c9c
Tom <tom@mademacbook-air.apollosrc.root>
2014-06-11 15:40:52 -0700

    overview changes recently

--- upload.php
+++ upload.php
@@ -5,17 +5,15 @@ if (isset($_FILES)) {
     if ($_FILES["file"]["error"] > 0) {
       echo "Error: " . $_FILES["file"]["error"] . "<br>";
     } else {
-        // move_uploaded_file($_FILES["file"]["tmp_name"], 'user_files/' . $_FILES["file"]["name"]);
 
-        // Make a database record for this file in a 'file' table
-
-        // So your first file will be file id 1
-        $file_id = 6;
-
-        // move file to be 1.gif ( or 1.png or whichever )
-
-
-        move_uploaded_file($_FILES["file"]["tmp_name"], 'user_files/' . $file_id . '.png');
+        if ($_FILES["file"]["name"]) {
+            $file_name = microtime(true) . $_FILES["file"]["name"];
+            move_uploaded_file($_FILES["file"]["tmp_name"], 'user_files/' . $file_name);
+            $img = '<img src=user_files/' . $file_name . ' height="100" width="160">';
+        } else {
+            $url = "http://imgs.tuts.dragoart.com/how-to-draw-a-river_1_000000007978_5.jpg";
+            $img = '<img src="' . $url . '" height="100" width="160">';
+        }
 
     }
 

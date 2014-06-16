4671d509773204a5d9c14c7ea8654b5c5948fb9b
fredrick-dominy <fredrickdominy@gmail.com>
2014-06-16 11:40:29 -0700

    updated flashcard.js functionality as well as structural cleanup

--- flashcard.php
+++ flashcard.php
@@ -4,10 +4,7 @@
   <title></title>
   <link href='http://fonts.googleapis.com/css?family=Unkempt:700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="css/flashcard-styles.css">
-
     <script src="js/jquery-2.1.1.min.js"></script>
-
-
     <script type="text/javascript">
       var deck =  <?php echo $jsonDoc; ?> ;
       // console.log(deck);
@@ -43,7 +40,7 @@
       <div class="answer1">
       </div>
       <div class="motivation">
-          <button class="go">GO</button>
+          <button class="go">NEXT!</button>
       </div>
       <div class="answer2">
       </div>

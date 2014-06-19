c448c7a1dc3ec1a7e7c1477b2f7c370ad129d709
fredrick-dominy <fredrickdominy@gmail.com>
2014-06-19 11:59:02 -0700

    updated and added fc-style to index.css

---
+++ fc_head.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+    <link href='http://fonts.googleapis.com/css?family=Graduate' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="css/index-styles.css">
+    <script src="js/jquery-2.1.1.min.js"></script>
+    <script src="js/jquery-ui-1.10.4.custom.min.js"></script>
+    <script type="text/javascript">
+      var deck =  <?php echo $jsonDoc; ?> ;
+    </script>
+    <script src="js/flashcard.js"></script>
+
+</head>
+
+<body>
--- flashcard.php
+++ flashcard.php
@@ -1,48 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title></title>
-  <link href='http://fonts.googleapis.com/css?family=Unkempt:700' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" type="text/css" href="css/flashcard-styles.css">
-    <script src="js/jquery-2.1.1.min.js"></script>
-    <script src="js/jquery-ui-1.10.4.custom.min.js"></script>
-    <script type="text/javascript">
-      var deck =  <?php echo $jsonDoc; ?> ;
-      // console.log(deck);
-    </script>
-    <script src="js/flashcard.js"></script>
 
-</head>
-
-<body>
-  <div class="page-container">
-
-    <div class="navigation">
-      <div class="title">
-        <h1>
-          FUN FLASH!
-        </h1>
-      </div>
-      <a class="nav" href="overview.php">HOME</a>
-      <div class="clear"></div>
-    </div>
+  <div class="fc-page-container">
 
     <div class="fc-container">
           <div class="flashcard">
-            <div class="indexCard">
-              <div class="side1"></div>
-              <div class="side2"></div>
+            <div class="fc-indexCard">
+              <div class="fc-side1"></div>
+              <div class="fc-side2"></div>
             </div>
           </div>
 
-        <div class="answer-block">
-          <div class="answer1"></div>
-          <div class="motivation"></div>
-          <div class="answer2"></div>
+        <div class="fc-answer-block">
+          <div class="fc-answer1"></div>
+          <div class="fc-motivation"></div>
+          <div class="fc-answer2"></div>
           <div class="clear"></div>
         </div>
     </div>
 
-    <div class="finish"></div>
+    <div class="fc-finish"></div>
 
   </div>

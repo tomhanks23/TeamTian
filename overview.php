562e7048da968cc1daa438593e18dbd5f66335cc
Deserae <deseraedias@gmail.com>
2014-06-20 12:24:08 -0700

    looking better

--- overview.php
+++ overview.php
@@ -41,36 +41,43 @@
       <!-- Card Creator Section with Drop Down Menu -->
       <div class="left">
 
-        Choose your deck:
+        <h4>Choose your deck:</h4>
         <select name="cardDeck" class="cardDeck">
           <?php 
             include ('getdeck.php');
            ?>
         </select>
         <br>
-        Card name(Right answer):
-        <input type="input" id="right_answer" placeholder="apple" value="" style="width:90px">
+        
 
       <!-- Text Area Card Content -->
         <!-- <textarea class="fc_front" rows="2" cols="20" placeholder="Front text."></textarea>
         <canvas id="canvas" style="border:1px solid black; height: 0px; width: 0px;"></canvas> -->
 
         <div class="fc_front">
+          <h5>Front of Card</h5>
+
           <form action="upload.php" id="imgform_front" method="post" enctype="multipart/form-data">
             <div id="imageloader" style="height:auto">
               <img src="http://imgs.tuts.dragoart.com/how-to-draw-a-river_1_000000007978_5.jpg" style="height:100px; width:160px">
             </div>
             <div id="view" style="height:auto"></div>
-            <input type="file" name="file" id="file" style="width:90px">
+            <input type="file" name="file" id="file">
           </form>
             <input type="input" id="front_link" style="width:130px" placeholder="Page link.">
           <textarea rows="2" cols="20" placeholder="What's this?" name="fc_front_text"></textarea>
-        </div>
 
-        Background color:
-        <input class="color" id="front_color" value="" style="width:90px"><br>
+          Background color:
+          <input class="color" id="front_color" value=""><br>
+
+        </div>
+        
+        <p>Card name(Right answer):</p>
+        <input type="input" id="right_answer" placeholder="apple" value="">
 
         <div  class="fc_back">
+          <h5>Back of Card</h5>
+
           <form action="upload.php" id="imgform_front" method="post" enctype="multipart/form-data">
             <div id="imageloader" style="height:auto">
               <img src="http://imgs.tuts.dragoart.com/how-to-draw-a-river_1_000000007978_5.jpg" style="height:100px; width:160px">
@@ -79,11 +86,12 @@
             <input type="file" name="file" id="file" style="width:90px"><br>
           </form>
             <input type="input" id="back_link" style="width:130px" placeholder="Page link.">
-          <textarea rows="2" cols="20" placeholder="Rockit?" name="fc_back_text"></textarea>
-        </div>
+          <textarea placeholder="Rockit?" name="fc_back_text"></textarea>
 
-        Background color:
-        <input class="color" id="back_color" value="" style="width:90px"><br>
+          Background color:
+          <input class="color" id="back_color" value=""><br>
+
+        </div>
 
         <button class="add" type="submit">Add New Card</button>
         
